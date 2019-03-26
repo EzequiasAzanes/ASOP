17cc0c89a14916a4c31d86f0e1a30025e6c7dae8
EzequiasAzanes <30569664+EzequiasAzanes@users.noreply.github.com>
2019-03-26 10:44:28 +0800

    Create Square.h

---
+++ Inheritance/Square.h
@@ -0,0 +1,11 @@
+//Square.h - declaration of Square class
+
+#include "Shape.h"
+
+//derived class
+class Square : public Shape
+{
+     public:
+         Square(int, int, int);    
+         void draw();   
+};
