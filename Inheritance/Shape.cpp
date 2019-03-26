881e532f7dbde44528a49839158f12870121b832
EzequiasAzanes <30569664+EzequiasAzanes@users.noreply.github.com>
2019-03-26 10:42:33 +0800

    Create Shape.cpp

---
+++ Inheritance/Shape.cpp
@@ -0,0 +1,10 @@
+//Shape.cpp - implementation/definition file of Shape
+
+#include "Shape.h"
+
+Shape::Shape(int x, int y, int size)
+{
+    this->x = x;
+    this->y = y;
+    this->size = size;
+}
